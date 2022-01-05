3a64a1729524caad41672bf24ca407551673f21d
Celso Vitorino <73009024+celsorv@users.noreply.github.com>
2022-01-05 17:42:45 -0300

    Create is_palindrome.js

---
+++ is_palindrome.js
@@ -0,0 +1,33 @@
+/***
+ *   Checks if string (phrase or word) is a palindrome
+ *   by Celso R. Vitorino
+ *
+ */
+
+function isPalindrome(str) {
+
+  tempStr = str.toLowerCase()
+               .normalize('NFD').replace(/[\u0300-\u036f]/g, '') // removes accents
+               .replace(/[^a-z0-9]/g, '')                        // removes special characters and blank spaces
+               .split('');
+
+  return tempStr.join('') === tempStr.reverse().join('');
+  
+}
+
+// testing the function
+
+strList = [
+  'arara', 
+  'ovo', 
+  'uva',
+  'osso', 
+  'A Daniela ama a lei? Nada!',
+  'banana',
+  'Luza Rocelina, a namorada do Manuel, leu na moda da romana: "anil é cor azul"'
+];
+
+console.log('\nÉ palíndromo:\n')
+
+for (let s of strList)
+  console.log(`${isPalindrome(s) ? '[ok]' : '[  ]'} ${s}`);
