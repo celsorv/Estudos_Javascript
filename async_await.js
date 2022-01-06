ff1ef59916ba544a28309e4bae36b0c3c6367838
Celso Vitorino <73009024+celsorv@users.noreply.github.com>
2022-01-06 18:51:18 -0300

    Update async_await.js

--- async_await.js
+++ async_await.js
@@ -1,18 +1,28 @@
+
 function primeiraFuncao() {
+
+    tempo = 2000    // 2000 ms
+
     return new Promise((resolve) => {
 
         setTimeout(() => {
-            console.log("Passou 1 segundo!")
+            
+            console.log(`Passaram-se ${tempo / 1000} segundo(s)`)
             resolve()
-        }, 1000)
+            
+        }, tempo)
 
     })
 }
 
 async function segundaFuncao() {
+    
     console.log("Iniciou")
+    
     await primeiraFuncao()
+    
     console.log("Terminou")
+    
 }
 
 segundaFuncao();
