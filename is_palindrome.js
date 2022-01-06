ab6cae75dd5b4de2d6eb756de2c6ea896533f085
Celso Vitorino <73009024+celsorv@users.noreply.github.com>
2022-01-06 04:28:38 -0300

    Update is_palindrome.js

--- is_palindrome.js
+++ is_palindrome.js
@@ -6,12 +6,11 @@
 
 function isPalindrome(str) {
   
-  // parameter validation --------------
-  if (!str) throw 'Invalid parameter.';
+  // parameter validation ----------------------------------------------------
+  if (!str) throw new InvalidParameterException();
   
-  if (typeof str !== 'string') 
-    throw 'Incompatible type parameter';
-  // -----------------------------------
+  if (typeof str !== 'string') throw new IncompatibleTypeParameterException();
+  // -------------------------------------------------------------------------
 
   tempStr = str.toLowerCase()
                .normalize('NFD').replace(/[\u0300-\u036f]/g, '') // removes accents
@@ -21,6 +20,35 @@ function isPalindrome(str) {
   return tempStr.join('') === tempStr.reverse().join('');
   
 }
+
+
+// user defined exceptions ----------------------------------
+function InvalidParameterException(message) {
+     if (!message) 
+        message = 'Parameter Value empty or null';
+        
+     this.message = message;
+     this.name = 'InvalidParameterException';
+     
+     this.toString = function() {
+         return `${this.name}: ${this.message}`;
+     }
+ }
+
+function IncompatibleTypeParameterException(message) {
+     if (!message) 
+        message = 'Parameter must be of type string';
+        
+     this.message = message;
+     this.name = 'IncompatibleTypeParameter';
+     
+     this.toString = function() {
+         return `${this.name}: ${this.message}`;
+     }
+ }
+// ---------------------------------------------------------
+
+
 
 // testing the function
 
