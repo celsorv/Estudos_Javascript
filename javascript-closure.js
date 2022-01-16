a4b8e9560431750f9d3ddfd316c1a3d67162c400
Celso Vitorino <73009024+celsorv@users.noreply.github.com>
2022-01-16 19:23:23 -0300

    Update javascript-closure.js

--- javascript-closure.js
+++ javascript-closure.js
@@ -1,20 +1,25 @@
 
-/* Entendendo Javascript Closures */
 
 /*
+    ------------------------------------------------------------------------------------------------------
     - a variável 'y' pertence ao escopo definido pela função sum().
     - a variável 'x' pertence ao escopo global, portanto, também acessível a partir da função sum().
+    ------------------------------------------------------------------------------------------------------
 */
 
 const x = 2;
+
 function sum() {
     const y = 3;
     return x + y;
 }
+
 console.log(sum());    // 5
 
 
-/*
+
+/*  
+    ------------------------------------------------------------------------------------------------------
     - seguindo o mesmo conceito citado acima, a função child() tem acesso à variável x da função parent().
     - a função parent() retorna uma referência para sua função interna child().
     - count faz referência à função child(), então não pode ser coletada pelo garbage collector.
@@ -23,6 +28,7 @@ console.log(sum());    // 5
 
     Closure: quando uma função é capaz de lembrar e acessar seu escopo léxico mesmo quando ela está
     sendo executada fora de seu escopo léxico.
+    -------------------------------------------------------------------------------------------------------
 */
 
 function parent(){
@@ -43,16 +49,27 @@ count();    // 2
 count();    // 3
 
 
- /*
+
+
+/*
+    ------------------------------------------------------------------------------------------------------
     Closures são aplicadas em estruturas de módulos.
- */
+    ------------------------------------------------------------------------------------------------------
+*/
 
 function counterModule() {
 
     let x = 0;
     
-    const increment = () => console.log(++x);   // função increment()
-    const decrement = () => console.log(--x);   // função decrement()
+    function increment() {      // const increment = () => console.log(++x);
+        x++;
+        console.log(x);
+    }
+    
+    function decrement() {      // const decrement = () => console.log(--x);
+        x--;
+        console.log(x);
+    }
 
     return {
         increment: increment,
